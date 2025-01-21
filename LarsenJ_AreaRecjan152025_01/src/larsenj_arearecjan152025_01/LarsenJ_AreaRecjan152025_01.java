a6ec779baa25149a6c93fcbccb7d83b79a67717b
jhlarsen1 <jhlarsen@live.com>
2025-01-21 11:36:29 -0500

    Add files via upload
    1st day of class area of a rectangle

---
+++ LarsenJ_AreaRecjan152025_01/src/larsenj_arearecjan152025_01/LarsenJ_AreaRecjan152025_01.java
@@ -0,0 +1,39 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package larsenj_arearecjan152025_01;
+
+import javax.swing.JOptionPane;
+/**
+ *
+ * @author larsenj8282
+ */
+public class LarsenJ_AreaRecjan152025_01 
+{
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) 
+    {
+        // TODO code application logic here
+        // Get the width of the rectangle from the user
+        String widthInput = JOptionPane.showInputDialog("Enter the width of the rectangle:");
+        // Convert the input to a double
+        double width = Double.parseDouble(widthInput);
+        
+        // Get the height of the rectangle from the user
+        String heightInput = JOptionPane.showInputDialog("Enter the height of the rectangle:");
+        // Convert the input to a double
+        double height = Double.parseDouble(heightInput);
+        
+        // Calculate the area
+        double area = width * height;
+        
+        // Display the result
+        JOptionPane.showMessageDialog(null, "The area of the rectangle is: " + area);
+    }
+}
+    
+
